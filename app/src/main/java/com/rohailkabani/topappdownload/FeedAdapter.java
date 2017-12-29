6bb835ff28b24a9a9911710d97e0b2726921d407
Rohail Kabani <rohail.kabani@gmail.com>
2017-12-29 11:56:38 -0500

    optimize views for memory

--- app/src/main/java/com/rohailkabani/topappdownload/FeedAdapter.java
+++ app/src/main/java/com/rohailkabani/topappdownload/FeedAdapter.java
@@ -35,18 +35,35 @@ public class FeedAdapter extends ArrayAdapter {
 
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View view = layoutInflater.inflate(layoutResource, parent, false);
-        TextView tvName = (TextView) view.findViewById(R.id.tvName);
-        TextView tvArtist = (TextView) view.findViewById(R.id.tvArtist);
-        TextView tvSummary = (TextView) view.findViewById(R.id.tvSummary);
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(layoutResource, parent, false);
+
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
 
         FeedEntry currentApp = application.get(position);
 
-        tvName.setText(currentApp.getName());
-        tvArtist.setText(currentApp.getArtist());
-        tvSummary.setText(currentApp.getSummary());
+        viewHolder.tvName.setText(currentApp.getName());
+        viewHolder.tvArtist.setText(currentApp.getArtist());
+        viewHolder.tvSummary.setText(currentApp.getSummary());
 
-        return view;
+        return convertView;
+    }
+
+    private  class ViewHolder {
+        final TextView tvName;
+        final TextView tvArtist;
+        final TextView tvSummary;
+
+        ViewHolder (View v) {
+            this.tvName = (TextView) v.findViewById(R.id.tvName);
+            this.tvArtist = (TextView) v.findViewById(R.id.tvArtist);
+            this.tvSummary = (TextView) v.findViewById(R.id.tvSummary);
+        }
     }
 
 
